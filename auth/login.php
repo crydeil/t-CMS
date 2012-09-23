534c56b45d5f1e1b1efd33601e1a25b7ec813d80
VikkyShostak <VikkyShostak@VikkyShostak-PC>
2012-09-24 00:58:38 +0400

    Version 0.1 beta2

--- auth/login.php
+++ auth/login.php
@@ -25,119 +25,123 @@ echo '<div class="container">
             <div class="span5">		
                 <h2>Вход для админа</h2>';
 
-                if (isset($_GET['logout']))
+    if (isset($_GET['logout']))
+    {
+
+            if (isset($_SESSION['user_id']))
+            {
+
+                unset($_SESSION['user_id']);
+
+            }
+
+            setcookie('login', '', 0, "/");
+            setcookie('password', '', 0, "/");
+
+            header('Location: ' . BASE_URL);
+            exit;
+
+    }
+
+    if (isset($_SESSION['user_id']))
+    {
+
+            header('Location: ' . BASE_URL . '/t-admin/index');
+            exit;
+
+    }
+
+    if (!empty($_POST))
+    {
+        $login = (isset($_POST['login'])) ? $db->real_escape_string($_POST['login']) : '';
+
+        $query = $db->query("SELECT `salt` FROM `t-users` WHERE `login`='{$login}' LIMIT 1");
+
+        if ($query->num_rows == 1)
+        {
+            $row = $query->fetch_assoc();
+
+            $salt = $row['salt'];					
+            $password = md5(md5($_POST['password']) . $salt);
+
+            $query = $db->query("SELECT `id` FROM `t-users` 
+                                                        WHERE `login`='{$login}' 
+                                                        AND `password`='{$password}' 
+                                                        LIMIT 1");
+
+            if ($query->num_rows === 1)
+            {                               
+                $row = $query->fetch_assoc();
+                $_SESSION['user_id'] = $row['id'];
+
+                $time = 86400;
+
+                if (isset($_POST['remember']))
                 {
-
-                        if (isset($_SESSION['user_id']))
-                        {
-
-                            unset($_SESSION['user_id']);
-
-                        }
-
-                        setcookie('login', '', 0, "/");
-                        setcookie('password', '', 0, "/");
-
-                        header('Location: ' . BASE_URL);
-                        exit;
-
+                    setcookie('login', $login, time()+$time, "/");
+                    setcookie('password', $password, time()+$time, "/");
                 }
 
-                if (isset($_SESSION['user_id']))
-                {
+                header('Location: ' . BASE_URL . '/t-admin/index');
+                exit;
 
-                        header('Location: ' . BASE_URL . '/t-admin/index');
-                        exit;
+            }
+            else
+            {
+                die('Такой логин с паролем не найдены в базе данных. 
+                    <a href="' . BASE_URL . '/auth/login">Попробовать ещё раз</a>.');
+            }
 
-                }
+        }
+        else
+        {
+            die('Пользователь с таким логином не найден. 
+                <a href="' . BASE_URL . '/auth/login">Попробовать ещё раз</a>.');
+        }
 
-                if (!empty($_POST))
-                {
-                    $login = (isset($_POST['login'])) ? $db->real_escape_string($_POST['login']) : '';
-
-                    $query = $db->query("SELECT `salt` FROM `t-users` WHERE `login`='{$login}' LIMIT 1");
-
-                    if ($query->num_rows == 1)
-                    {
-                        $row = $query->fetch_assoc();
-
-                        $salt = $row['salt'];					
-                        $password = md5(md5($_POST['password']) . $salt);
-
-                        $query = $db->query("SELECT `id` FROM `t-users` 
-                                                                    WHERE `login`='{$login}' 
-                                                                    AND `password`='{$password}' 
-                                                                    LIMIT 1");
-
-                        if ($query->num_rows === 1)
-                        {                               
-                            $row = $query->fetch_assoc();
-                            $_SESSION['user_id'] = $row['id'];
-
-                            $time = 86400;
-
-                            if (isset($_POST['remember']))
-                            {
-                                setcookie('login', $login, time()+$time, "/");
-                                setcookie('password', $password, time()+$time, "/");
-                            }
-
-                            header('Location: ' . BASE_URL . '/t-admin/index');
-                            exit;
-                            
-                        }
-                        else
-                        {
-                            die('Такой логин с паролем не найдены в базе данных. <a href="' . BASE_URL . '/auth/login">Попробовать ещё раз</a>.');
-                        }
-                        
-                    }
-                    else
-                    {
-                        die('Пользователь с таким логином не найден. <a href="' . BASE_URL . '/auth/login">Попробовать ещё раз</a>.');
-                    }
-                    
-                }
+    }
 
 			
 
-                echo '<form action="" method="post">
+    echo '<form action="" method="post">
 
-                        <div class="input-prepend">
-                            <span class="add-on"><i class="icon-user"></i></span><input type="text" class="input-xlarge" name="login" placeholder="Ваш логин" />
-                        </div>
-                        <p></p>
-                        <div class="input-prepend">
-                            <span class="add-on"><i class="icon-key"></i></span><input type="password" class="input-xlarge" name="password" placeholder="Ваш пароль" />
-                        </div>
-                        <p></p>
-                        <p><label class="checkbox inline">
-                                <input type="checkbox" id="inlineCheckbox1" name="remember"> запомнить меня на 24 часа!
-                        </label></p>
-                       
-                        <p><input type="submit" value="Войти в панель администратора" class="btn btn-primary" /></p>
-
-                    </form>
-
+            <div class="input-prepend">
+                <span class="add-on"><i class="icon-user"></i></span>
+                <input type="text" class="input-xlarge" name="login" placeholder="Ваш логин" />
             </div>
-
-            <div class="span7">
-
-                    <h3>Информация</h3>
-                    <p>В этом разделе ты сможешь войти в панель администратора. Доступен следующий функционал:
-                    <ul>
-                        <li>добавление новых, редактирование существующих и удаление лишних записей/страниц;</li>
-                        <li>создание и редактирование меню/пунктов меню;</li>
-                        <li>редактирование информации на главной странице;</li>
-                        <li>и многое другое...</li>
-                    </ul>
-                    </p>
-                    <p>Если ты попал на эту страницу случайно - переходи на <a href="' . BASE_URL . '">главную</a>.</p>
+            <p></p>
+            <div class="input-prepend">
+                <span class="add-on"><i class="icon-key"></i></span>
+                <input type="password" class="input-xlarge" name="password" placeholder="Ваш пароль" />
             </div>
+            <p></p>
+            <p><label class="checkbox inline">
+                    <input type="checkbox" id="inlineCheckbox1" name="remember"> запомнить меня на 24 часа!
+            </label></p>
 
-        </div>
+            <p><input type="submit" value="Войти в панель администратора" class="btn btn-primary" /></p>
 
-    </div>';
+        </form>
+
+    </div>
+
+    <div class="span7">
+
+            <h3>Информация</h3>
+            <p>В этом разделе ты сможешь войти в панель администратора. Доступен следующий функционал:
+            <ul>
+                <li>добавление новых, редактирование существующих и удаление лишних записей/страниц;</li>
+                <li>создание и редактирование меню/пунктов меню;</li>
+                <li>редактирование информации на главной странице;</li>
+                <li>и многое другое...</li>
+            </ul>
+            </p>
+            <p>Если ты попал на эту страницу случайно - переходи на <a href="' . BASE_URL . '">главную</a>.</p>
+    </div>
+
+</div>
+
+</div>';
 
 echo $footer->getAdmin();
             
--- first-install.php
+++ first-install.php
@@ -24,115 +24,115 @@ echo '<div class="container">
             <div class="span5">
                 <h2>Введите свои данные</h2>';
 
-		function GenerateSalt($n=3)
-		{
-			$key = '';
-			$pattern = '1234567890abcdefghijklmnopqrstuvwxyz.,*_-=+';
-			$counter = strlen($pattern)-1;
-			for($i=0; $i<$n; $i++)
-			{
-				$key .= $pattern{rand(0,$counter)};
-			}
-			return $key;
-		}
+function GenerateSalt($n=3)
+{
+        $key = '';
+        $pattern = '1234567890abcdefghijklmnopqrstuvwxyz.,*_-=+';
+        $counter = strlen($pattern)-1;
+        for($i=0; $i<$n; $i++)
+        {
+                $key .= $pattern{rand(0,$counter)};
+        }
+        return $key;
+}
 
-		if (empty($_POST))
-		{
-                    echo '<form action="" method="post">
-                                <div class="input-prepend">
-                                    <span class="add-on"><i class="icon-user"></i></span>
-                                    <input type="text" class="input-xlarge" placeholder="Логин администратора" name="login" />
-                                </div>
-                                <div class="input-prepend">
-                                    <span class="add-on"><i class="icon-key"></i></span>
-                                    <input type="password" class="input-xlarge" placeholder="Пароль администратора" name="password" />
-                                </div>
-                                <div class="input-prepend">
-                                    <span class="add-on"><i class="icon-key"></i></span>
-                                    <input type="password" class="input-xlarge" placeholder="Пароль ещё раз" name="password_confirm" /></p>
-                                </div>
-                                Будьте внимательны!<br /> Пароль админа восстановить невозможно.</p>
-                                <p><input type="submit" class="btn btn-primary" value="Установить t-CMS" /></p>
-                            </form>';
+if (empty($_POST))
+{
+    echo '<form action="" method="post">
+                <div class="input-prepend">
+                    <span class="add-on"><i class="icon-user"></i></span>
+                    <input type="text" class="input-xlarge" placeholder="Логин администратора" name="login" />
+                </div>
+                <div class="input-prepend">
+                    <span class="add-on"><i class="icon-key"></i></span>
+                    <input type="password" class="input-xlarge" placeholder="Пароль администратора" name="password" />
+                </div>
+                <div class="input-prepend">
+                    <span class="add-on"><i class="icon-key"></i></span>
+                    <input type="password" class="input-xlarge" placeholder="Пароль ещё раз" name="password_confirm" /></p>
+                </div>
+                Будьте внимательны!<br /> Пароль админа восстановить невозможно.</p>
+                <p><input type="submit" class="btn btn-primary" value="Установить t-CMS" /></p>
+            </form>';
 
-                }
-                else
-                {
-			
-                    $login = (isset($_POST['login'])) ? $db->real_escape_string($_POST['login']) : '';
-                    $password = (isset($_POST['password'])) ? $db->real_escape_string($_POST['password']) : '';
-                    $password_confirm = (isset($_POST['password_confirm'])) ? $db->real_escape_string($_POST['password_confirm']) : '';
+}
+else
+{
 
-                    $error = false;
-                    $errort = '';
+    $login = (isset($_POST['login'])) ? $db->real_escape_string($_POST['login']) : '';
+    $password = (isset($_POST['password'])) ? $db->real_escape_string($_POST['password']) : '';
+    $password_confirm = (isset($_POST['password_confirm'])) ? $db->real_escape_string($_POST['password_confirm']) : '';
 
-                    if (strlen($login) < 3)
-                    {
-                            $error = true;
-                            $errort .= '- Длина логина должна быть не менее 3-х символов.<br />';
-                    }
-                    if (strlen($password) < 6)
-                    {
-                            $error = true;
-                            $errort .= '- Длина пароля должна быть не менее 6-ти символов.<br />';
-                    }
-                    if ($password !== $password_confirm)
-                    {
-                            $error = true;
-                            $errort .= '- Пароли не совпадают.<br />';
-                    }
-		
-                    $query = $db->query("SELECT `id` FROM `t-users` WHERE `login`='{$login}' LIMIT 1");
-                    
-                    if ($query->num_rows === 1)
-                    {
-                        $error = true;
-                        $errort .= '- Пользователь с таким логином уже существует в базе данных, введите другой.<br />';
-                    }
-				
-                    if (!$error)
-                    {
+    $error = false;
+    $errort = '';
 
-                        $salt = GenerateSalt();
-                        $hashed_password = md5(md5($password) . $salt);
+    if (strlen($login) < 3)
+    {
+            $error = true;
+            $errort .= '- Длина логина должна быть не менее 3-х символов.<br />';
+    }
+    if (strlen($password) < 6)
+    {
+            $error = true;
+            $errort .= '- Длина пароля должна быть не менее 6-ти символов.<br />';
+    }
+    if ($password !== $password_confirm)
+    {
+            $error = true;
+            $errort .= '- Пароли не совпадают.<br />';
+    }
 
-                        $query = $db->query("INSERT INTO `t-users` SET
-                                                                    `login`='{$login}',
-                                                                    `password`='{$hashed_password}',
-                                                                    `salt`='{$salt}'");
-                        
-                        echo '<h4>Поздравляем, Вы успешно зарегистрированы!</h4><a href="' . BASE_URL . '/auth/login">Авторизоваться</a>';
-                        
-                    }
-                    else
-                    {
-                        
-                        echo '<h4>Возникли следующие ошибки</h4>' . $errort . '<p></p><p>Вернуться назад и <a href="' . BASE_URL . '/first-install.php">попробовать ещё раз</a>.</p>';
-                                            
-                    }
-                }
-                
-                echo '</div>
-        
-                        <div class="span7">
-                            <h3>Инструкции</h3>
-                            <p>Для успешной инсталляции <strong>t-CMS</strong> - заполни файл <em>./ t-admin/config.php</em> (в корне) в соответствии с твоим хостингом.</p>
-                            <p>Если ты уже установил <strong>t-CMS</strong>, то:</p> 
-                            <p>
-                            <ol>
-                                <li>удали файл <em>./ first-install.php</em>;</li>
-                                <li>поставь права CHMOD 777 на папку <em>./ uploads</em> (в корне);</li>
-                                <li>войди в <a href="' . BASE_URL . '/auth/login">панель управления</a> и управляй своим сайтом.</li>
-                            </ol>
-                            </p>
-                            <p>Успехов!</p>
-                        </div>
+    $query = $db->query("SELECT `id` FROM `t-users` WHERE `login`='{$login}' LIMIT 1");
 
-                        
+    if ($query->num_rows === 1)
+    {
+        $error = true;
+        $errort .= '- Пользователь с таким логином уже существует в базе данных, введите другой.<br />';
+    }
 
-                    </div>
+    if (!$error)
+    {
 
-                </div>';
+        $salt = GenerateSalt();
+        $hashed_password = md5(md5($password) . $salt);
+
+        $query = $db->query("INSERT INTO `t-users` SET
+                                                    `login`='{$login}',
+                                                    `password`='{$hashed_password}',
+                                                    `salt`='{$salt}'");
+
+        echo '<h4>Поздравляем, Вы успешно зарегистрированы!</h4><a href="' . BASE_URL . '/auth/login">Авторизоваться</a>';
+
+    }
+    else
+    {
+
+        echo '<h4>Возникли следующие ошибки</h4>' . $errort . '<p></p><p>Вернуться назад и <a href="' . BASE_URL . '/first-install.php">попробовать ещё раз</a>.</p>';
+
+    }
+}
+
+echo '</div>
+
+        <div class="span7">
+            <h3>Инструкции</h3>
+            <p>Для успешной инсталляции <strong>t-CMS</strong> - заполни файл <em>./ t-admin/config.php</em> (в корне) в соответствии с твоим хостингом.</p>
+            <p>Если ты уже установил <strong>t-CMS</strong>, то:</p> 
+            <p>
+            <ol>
+                <li>удали файл <em>./ first-install.php</em>;</li>
+                <li>поставь права CHMOD 777 на папку <em>./ uploads</em> (в корне);</li>
+                <li>войди в <a href="' . BASE_URL . '/auth/login">панель управления</a> и управляй своим сайтом.</li>
+            </ol>
+            </p>
+            <p>Успехов!</p>
+        </div>
+
+
+
+    </div>
+
+</div>';
 
 echo $footer->getAdmin();
 
