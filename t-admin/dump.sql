574511254fc4593e5e843741624758a76faf53a0
VikkyShostak <VikkyShostak@VikkyShostak-PC>
2012-09-28 21:44:35 +0400

    Version 0.1 beta8 new dump.sql

--- t-admin/dump.sql
+++ t-admin/dump.sql
@@ -1,8 +1,18 @@
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
+CREATE TABLE IF NOT EXISTS `t-comments` (
+  `id` smallint(11) NOT NULL AUTO_INCREMENT,
+  `comment_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `comment_content_url` varchar(512) NOT NULL,
+  `comment_author` varchar(512) NOT NULL,
+  `comment_author_email` varchar(512) NOT NULL,
+  `comment_body` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+
 CREATE TABLE IF NOT EXISTS `t-content` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` smallint(11) NOT NULL AUTO_INCREMENT,
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `title` varchar(512) NOT NULL,
   `url` varchar(512) NOT NULL,
@@ -11,33 +21,33 @@ CREATE TABLE IF NOT EXISTS `t-content` (
   `meta_keywords` text NOT NULL,
   `meta_description` text NOT NULL,
   `tags` text NOT NULL,
-  `is_page` int(11) NOT NULL DEFAULT '0',
+  `is_page` smallint(2) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
 
 CREATE TABLE IF NOT EXISTS `t-menu` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` smallint(11) NOT NULL AUTO_INCREMENT,
   `menu_title` varchar(512) NOT NULL,
-  `menu_url` text NOT NULL,
+  `menu_url` varchar(512) NOT NULL,
   `menu_name` varchar(512) NOT NULL,
-  `item_order` int(11) NOT NULL DEFAULT '0',
+  `item_order` smallint(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
 
 CREATE TABLE IF NOT EXISTS `t-settings` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` smallint(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(512) NOT NULL,
   `body` text NOT NULL,
   `meta_keywords` text NOT NULL,
   `meta_description` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
 
 CREATE TABLE IF NOT EXISTS `t-users` (
-  `id` smallint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `id` smallint(11) unsigned NOT NULL AUTO_INCREMENT,
   `login` varchar(50) NOT NULL DEFAULT '',
   `password` varchar(32) NOT NULL DEFAULT '',
   `salt` char(3) NOT NULL DEFAULT '',
   PRIMARY KEY (`id`),
   UNIQUE KEY `login` (`login`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
